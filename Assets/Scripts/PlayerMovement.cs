9992388a28a49e64fc35d28fed1b61300d21128f
kimia khabiri <kimia.khabiri.92@gmail.com>
2022-09-03 09:38:25 +0430

    Initial Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{    
+    private Rigidbody rb;
+    private BoxCollider coll;
+    [SerializeField] private Transform GroundCheck;
+    [SerializeField] private LayerMask Ground;
+    [SerializeField] private float MoveSpeed = 8f;
+    [SerializeField] private float JumpForce = 7f;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        coll = GetComponent<BoxCollider>();
+    }
+
+    private void Update()
+    {
+        float dirX = Input.GetAxis("Horizontal");
+        float dirZ = Input.GetAxis("Vertical");
+        rb.velocity = new Vector3 (dirX * MoveSpeed, rb.velocity.y, dirZ * MoveSpeed);
+
+        if (Input.GetButtonDown("Jump") && isGrounded()){
+            rb.velocity   = new Vector3(rb.velocity.x, JumpForce, rb.velocity.z);
+        }
+    }
+
+    private bool isGrounded()
+    {
+        return Physics.CheckSphere(GroundCheck.position, .1f, Ground);
+    }
+}
