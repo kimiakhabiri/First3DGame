fc5d3024f93816abda8d36961ac4f3abe16ccd7a
--unset <kimiakhabiri01@gmail.com>
2022-09-07 17:42:33 +0430

    Count Collected Items in UI

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
     int coins = 0;
+    [SerializeField] Text coinsText;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Coin"))
         {
             Destroy(other.gameObject);
             coins++;
-            Debug.Log("Coins: " + coins);
+            coinsText.text = "Coins: " + coins;
         }
     }
 }
